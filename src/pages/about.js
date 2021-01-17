6f4a26c8e79c82f49d471620aa0b737e80599a9c
ianholden123 <ianholden123@hotmail.co.uk>
2021-01-17 23:12:24 +0000

    :mag: Added Person schema to about and home pages.

--- src/pages/about.js
+++ src/pages/about.js
@@ -19,16 +19,16 @@ const AboutPage = () => (
       description='Ian Holden is a software Engineer based in Kent, UK. Get in touch to see how Ian can help you build your next web project.'
       pathName='/about/'
     />
-    <article id='aboutWrapper' className='block-center py-6'>
+    <article id='aboutWrapper' className='block-center py-6' itemScope itemType="http://schema.org/Person">
       <h1 className='not-full-width block-center px-4'>About Me</h1>
       <div id='general-info' className='grid'>
         <div id='ian-image' className='grid-panel' />
         <div className='grid-panel not-full-width px-4'>
-          <h2 className='mb-3'>My name is Ian Holden.</h2>
+          <h2 className='mb-3'>My name is <span itemProp="givenName">Ian</span> <span itemProp="familyName">Holden</span>.</h2>
           <h3>I&apos;m a Software Engineer.</h3>
           <p>I started my professional career as an Optimisation Executive after completing my degree in Computer Science.</p>
           <p>I gained skills, working with in-house and external Analytics tools before joining a small Research &amp; Development team. As an R&amp;D Engineer, I built hybrid applications using Ionic with Angular.</p>
-          <p>I am currently working as a Software Engineer, developing frontend systems and managing integrations within a microservices architecture.</p>
+          <p>I am currently working as a <span itemProp="jobTitle">Software Engineer</span>, developing frontend systems and managing integrations within a microservices architecture.</p>
         </div>
       </div>
       <h2 className='mt-6 px-4 not-full-width block-center'>Technologies that I have experience with</h2>
